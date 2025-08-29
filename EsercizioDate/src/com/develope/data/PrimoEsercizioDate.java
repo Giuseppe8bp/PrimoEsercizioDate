f0465d52fc507e9c496db5c82df9483cdf16aab3
maria <mariamacrisecret@gmail.com>
2025-08-29 15:50:57 +0200

    first commit

---
+++ EsercizioDate/src/com/develope/data/PrimoEsercizioDate.java
@@ -0,0 +1,29 @@
+package com.develope.data;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+public class PrimoEsercizioDate {
+
+	public static void main(String[] args) {
+
+		OffsetDateTime data= OffsetDateTime.parse("2002-03-01T13:00:00Z");
+		
+		System.out.println(data);
+		
+		String dateString = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
+		
+		System.out.println(dateString);
+		
+		String dateString1 = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+		System.out.println(dateString1);
+		
+		String dateString2 = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
+		System.out.println(dateString2);
+		
+		
+		
+	}
+}
+
+
